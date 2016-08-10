1ecd75f5c1e306655e9a9a4c5f7d580223ac1cd8
Olli Raitio <olli.raitio@helsinki.fi>
2016-08-10 20:38:01 +0300

    fix notifyd & muttattach

--- scripts/mutt/muttattach.sh
+++ scripts/mutt/muttattach.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-ssh robopop "mkdir /tmp/muttattach"
+ssh robopop "[[ -d /tmp/muttattach ]] || mkdir /tmp/muttattach"
 ssh robopop "touch /tmp/muttattach/filename_fifo"
 ssh robopop "tail -f -n 0 /tmp/muttattach/filename_fifo" | while read line; do
 	file=$(basename $line)
+	notify-send "Fetching" "$file"
 	scp robopop:/tmp/muttattach/$file /tmp/
 	ssh robopop "rm /tmp/muttattach/$file" &
 	xdg-open /tmp/$file

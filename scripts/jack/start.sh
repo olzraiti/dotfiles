1d843780eeaa58e048e482b4a7477b37e8ac4119
Olli Raitio <olli.raitio@helsinki.fi>
2020-09-22 20:14:40 +0300

    tmux, jack

--- scripts/jack/start.sh
+++ scripts/jack/start.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-jack_control dps rate 48000
-pacmd set-default-sink "jack_out"
-killall pulseaudio
+$HOME/effect-tools/bin/start_jack
+#jack_control dps rate 48000
+#pacmd set-default-sink "jack_out"
+#killall pulseaudio
 
-jack_connect system:capture_2 system:playback_1
-jack_connect system:capture_2 system:playback_2
+#jack_connect system:capture_2 system:playback_1
+#jack_connect system:capture_2 system:playback_2
 
-patchage -A &
+#patchage -A &
 
-qsynth & 1>/dev/null 2>&1
-while [[ -z $(jack_lsp -c | grep '^qsynth') ]]; do
-	echo sleep
-	sleep 1
-done
-aconnect 24 128
+#qsynth & 1>/dev/null 2>&1
+#while [[ -z $(jack_lsp -c | grep '^qsynth') ]]; do
+#	echo sleep
+#	sleep 1
+#done
+#aconnect 24 128
 a2jmidid -e & 1>/dev/null 2>&1
